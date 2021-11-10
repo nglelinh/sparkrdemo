fd06f4f017c26ff25edf5ec4e929c87b3a361fda
giangcs <songiang@gmail.com>
2021-11-10 14:13:57 +0700

    add function set current_position as latest job's name in job history of candidate

--- Sparkr/Domain/ProfileManagement/PersonalProfile/Services/PersonalProfileDomainService.php
+++ Sparkr/Domain/ProfileManagement/PersonalProfile/Services/PersonalProfileDomainService.php
@@ -5,6 +5,7 @@ namespace Sparkr\Domain\ProfileManagement\PersonalProfile\Services;
 //use Sparkr\Domain\Base\Exception\BaseValidationException;
 
 
+use Illuminate\Support\Collection;
 use Sparkr\Domain\ProfileManagement\PersonalProfile\Interfaces\PersonalProfileRepositoryInterface;
 
 class PersonalProfileDomainService
@@ -15,6 +16,21 @@ class PersonalProfileDomainService
         PersonalProfileRepositoryInterface $personalProfileRepository
     ) {
         $this->personalProfileRepository = $personalProfileRepository;
+    }
+
+    // Get current position as latest job's name
+    public function getCurrentJob(Collection $jobHistory): string
+    {
+        $jobName = '';
+        $latestJobDate = $jobHistory->first()->getEndDate();
+        foreach ($jobHistory as $job){
+            $jobEndDate = $job->getEndDate();
+            if ($jobEndDate->greaterThan($latestJobDate)){
+                $latestJobDate = $jobEndDate;
+                $jobName = $job->getTitle();
+            }
+        }
+        return $jobName;
     }
 
 }
